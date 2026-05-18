ffb0aabb57e476af80fdabd83543eacad052c649
sybdz <207881135+sybdz@users.noreply.github.com>
2026-05-18 21:52:18 +0800

    ci: add material3 test dependencies

--- material3/build.gradle.kts
+++ material3/build.gradle.kts
@@ -30,4 +30,8 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
